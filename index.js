7326979c0281eb583b8f803fbff501518f0b18cf
Maximiliano Martin <maxinirbeat@gmail.com>
2023-05-16 01:38:36 -0300

    ste commit solo existe para probar si eliminando el dns.lookup anda desde el deploy

--- index.js
+++ index.js
@@ -46,24 +46,24 @@ app.post('/api/shorturl', (req, res, next) => {
         original_url: req.body.url,
         short_url: (count + 1).toString()
       })
-      return urlToSave
+      // return urlToSave
 
-    }).then(urlToSave=>{
-      let queryUrl= urlToSave.original_url.slice(urlToSave.original_url.indexOf("w"));
+    // }).then(urlToSave=>{
+    //   let queryUrl= urlToSave.original_url.slice(urlToSave.original_url.indexOf("w"));
 
-      dns.lookup(queryUrl,(err,address,family)=>{
-        if(!err){
+      // dns.lookup(queryUrl,(err,address,family)=>{
+      //   if(!err){
           urlToSave.save();
           res.json({
             original_url: urlToSave.original_url,
             short_url: parseInt(urlToSave.short_url)
           })
-        }else {
-          res.json({
-            error: 'invalid url'
-          })
-        }
-      })
+  //       }else {
+  //         res.json({
+  //           error: 'invalid url'
+  //         })
+  //       }
+  //     })
     })
   }
   else {
